8115f6834f6cd260e03f070e5bf4964bafda04bc
conor <conor@Aragorn>
2026-07-05 16:10:46 -0600

    Commit and push for book

--- LibrarySystemPortfolio/src/librarysystem/Book.java
+++ LibrarySystemPortfolio/src/librarysystem/Book.java
@@ -1,5 +1,132 @@
 package librarysystem;
 
 public class Book {
+	
+	
+	private int id;
+	private String title;
+	private String author;
+	private String isbn;
+	private int pages;
+	
+	
+	
+	
+	public Book() {}
+		
+		
+		public Book(int id, String title, String author, String isbn, int pages) {
+			
+			
+			this.id = id;
+			this.title = title;
+			this.author = author;
+			this.isbn = isbn;
+			this.pages = pages;
+			
+		
+		
+		}
+		
+		
+		public int getId() {
+			
+			
+			return id;
+			
+		}
+		
+		public String getTitle() {
+			
+			
+			return title;
+			
+		}
+		
+		
+		public String getAuthor() {
+			
+			
+			return author;
+			
+			
+		
+		}
+		
+		public String getIsbn() {
+			
+			
+			return isbn;
+			
+		}
+		
+		
+		public int getPages() {
+			
+			
+			return pages;
+			
+		}
+		
+		
+		
+		public void setId(int id) {
+			
+			this.id = id;
+			
+		}
+		
+		
+		public void setTitle(String title) {
+			
+			this.title = title;
+			
+			
+		}
+		
+		public void setAuthor(String author) {
+			
+			
+			this.author = author;
+			
+		}
+		
+		public void setIsbn(String isbn) {
+			
+			
+			this.isbn = isbn;
+			
+			
+		}
+		
+		
+		public void setPages(int pages) {
+			
+			
+			this.pages = pages;
+			
+			
+			
+		}
+		
+		
+		public void printBookInfo() {
+		
+		
+		System.out.println("-------------------------");
+		System.out.println("ID: " + id);
+		System.out.println("Title: " + title);
+		System.out.println("Author: " + author);
+		System.out.println("ISBN: " + isbn);
+		System.out.println("Pages: " + pages);
+		
+		
+		
+		}
+		
+		
+		
+	}
+	
 
-}
+
