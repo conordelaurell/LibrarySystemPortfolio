c05c20d035d03a674872b413d5243c1cb9759445
conor <conor@Aragorn>
2026-07-05 16:22:35 -0600

    First push and second commit for inventory

--- LibrarySystemPortfolio/src/librarysystem/Inventory.java
+++ LibrarySystemPortfolio/src/librarysystem/Inventory.java
@@ -1,5 +1,85 @@
 package librarysystem;
 
-public class Inventory {
 
+import java.util.ArrayList;
+
+public class Inventory {
+	
+	private ArrayList<Book> inventory;
+	private ArrayList<Book> borrowedBooks;
+	
+	public Inventory() {
+		
+		inventory = new ArrayList<>();
+		borrowedBooks = new ArrayList<>();
+		
+		
+		
+	}
+	
+	
+	public void addBook(Book book) {
+		
+		inventory.add(book);
+		System.out.println("Book added to the library!");
+		
+		
+		
+	}
+	
+	
+	public boolean borrowBook(int id) {
+		
+		for(int i = 0; i < inventory.size(); i++) {
+			
+			
+			if(inventory.get(i).getId() == id) {
+				
+				borrowedBooks.add(inventory.get(i));
+				inventory.remove(i);
+				System.out.println("Book successfully borrowed");
+				return true;
+				
+				
+			}
+			
+			
+			
+		}
+		
+		System.out.println("Book not found or already borrowed!");
+		return false;
+		
+	}
+	
+	public boolean returnBook(int id) {
+		
+		for (int i = 0; i < borrowedBooks.size(); i++) {
+			
+			if(borrowedBooks.get(i).getId() == id) {
+				
+				
+				inventory.add(borrowedBooks.get(i));
+				borrowedBooks.remove(i);
+				System.out.println("Book successfully returns");
+				return true;
+			}
+			
+			
+		}
+		
+		System.out.println("Book is not currently borrowed!");
+		return false;
+		
+		
+	}
+	
+	public void printAll() {
+		
+		
+		
+		
+	}
+	
+	
 }
