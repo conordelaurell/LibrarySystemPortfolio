8dba2cea0ebffd9dd001f6b08480408eb0b28511
conor <conor@Aragorn>
2026-07-05 19:21:26 -0600

    Commit and Push 1 for Library System

--- LibrarySystemPortfolio/src/librarysystem/LibrarySystem.java
+++ LibrarySystemPortfolio/src/librarysystem/LibrarySystem.java
@@ -1,5 +1,41 @@
 package librarysystem;
 
+
+import java.util.Scanner;
+
 public class LibrarySystem {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		Inventory inventory = new Inventory();
+		int choice = 0;
+		
+		
+		
+		while (choice != 0) {
+			
+			System.out.println("\n------ Libray Menu ------");
+			System.out.println("1. Add Book");
+			System.out.println("2. Borrow Book");
+			System.out.println("3. Return Book");
+			System.out.println("4. Search by Title");
+			System.out.println("5. Print All Books");
+			System.out.println("6. Exit");
+			System.out.println("Enter your choice: ");
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+	}
 
 }
